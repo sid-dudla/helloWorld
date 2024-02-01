96cd7b55f7a1b4c7bc90c82b6e90a6e7faeb75c3
Siddharth Dudla <sdudla@terpmail.umd.edu>
2024-01-31 20:10:29 -0500

    Added code to demonstrate a first code change.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Siddharth Dudla!'
+    return 'Hello World from Siddharth Dudla! I am adding my first change!'
 
 
 if __name__ == '__main__':
